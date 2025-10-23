40f4abc39a7a42f35fb1992134f1a222ad473c14
brentirmak <brent.irmak@gmail.com>
2025-10-23 11:47:17 -0700

    Refactor security group setup for blog instance
    Updated the security group configuration for the blog instance by replacing the module with a direct resource definition. Added ingress and egress rules for HTTP, HTTPS, and all outbound traffic.

--- main.tf
+++ main.tf
@@ -22,74 +22,46 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  #vpc_security_group_ids = [aws_security_group.blog.id]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning Terraform"
   }
 }
 
-
-
-module "blog_sg" {
-  source      = "terraform-aws-modules/security-group/aws"
-  version     = "4.13.0"
-  name = "blog_new"
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules       = ["all/all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-#module "blog_sg" {
-#  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
-#  name        = "web-server"
-#  description = "Security group for web-server with HTTP ports open within VPC"
-  #vpc_id      = "vpc-12345678"
-#  vpc_id = data.aws_vpc.default.id
+  security_group_id = aws_security_group.blog.id
+}
 
-#  ingress_cidr_blocks = ["10.10.0.0/16"]
-#}
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
+  security_group_id = aws_security_group.blog.id
+}
 
-#resource "aws_security_group" "blog" {
-#  name        = "blog"
-#  description = "Allow http and https in. Allow everything out"
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 
-#  vpc_id = data.aws_vpc.default.id
-#}
-
-#resource "aws_security_group_rule" "blog_http_in" {
-#  type        = "ingress"
-#  from_port   = 80
-#  to_port     = 80
-#  protocol    = "tcp"
-#  cidr_blocks = ["0.0.0.0/0"]
-
-#  security_group_id = aws_security_group.blog.id
-#}
-
-#resource "aws_security_group_rule" "blog_https_in" {
-#  type        = "ingress"
-#  from_port   = 443
-#  to_port     = 443
-#  protocol    = "tcp"
-#  cidr_blocks = ["0.0.0.0/0"]
-
-#  security_group_id = aws_security_group.blog.id
-#}
-
-#resource "aws_security_group_rule" "blog_everything_out" {
-#  type        = "egress"
-#  from_port   = 0
-#  to_port     = 0
-#  protocol    = "-1"
-#  cidr_blocks = ["0.0.0.0/0"]
-
-#  security_group_id = aws_security_group.blog.id
-#}
+  security_group_id = aws_security_group.blog.id
+}
